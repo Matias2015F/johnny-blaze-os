85e6389698ac405fa7d94bbae7f177b091e799c0
Matias2015F <matias4604@gmail.com>
2026-05-30 22:10:16 -0300

    fix: useMemo import y meta mobile-web-app-capable

--- src/components/OrderDetailView.jsx
+++ src/components/OrderDetailView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { ArrowLeft, Bell, CheckCircle2, ChevronDown, ClipboardList, Clock, DollarSign, Edit2, FileText, Play, Search, Send, ShieldCheck, ThumbsUp, Trash2, Truck, Wrench, X } from "lucide-react";
 import { LS, buscarRepuestosAutocomplete, crearEntradaHistorial, generateId, guardarRepuestoHistorial } from "../lib/storage.js";
 import { buildProximoControl } from "../lib/proximoControl.js";
